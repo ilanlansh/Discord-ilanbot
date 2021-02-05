910c79d8c1ec5f0034ddac791eb481de7e399e38
ilan <ilan.shrir@gmail.com>
2021-02-05 15:30:00 +0200

    update ,,ping command, remove ,,pong command

--- commands/ping.js
+++ commands/ping.js
@@ -4,6 +4,10 @@ module.exports =
     description: "this is a ping command!",
     execute(message, args)
     {
-        message.channel.send("pong!");
+        message.channel.send("Pong!").then(m =>
+            {
+                let ping = m.createdTimestamp - message.createdTimestamp;
+                m.edit(`Pong! **${ping}ms**`);
+            });
     }
 }
--- commands/pong.js
+++
@@ -1,9 +0,0 @@
-module.exports = 
-{
-    name: 'pong',
-    description: "this is a pong command!",
-    execute(message, args)
-    {
-        message.channel.send("ping!");
-    }
-}
